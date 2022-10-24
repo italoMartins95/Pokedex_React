d33f53ae08c8db78d3f5af865c3a318651df6c00
italoMartins95 <italomartinssk88@gmail.com>
2022-10-24 05:00:28 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <>
     <Router>         
       <Routes>
-        <Route exact path='/' element={<Container />} />
+        <Route path='/' element={<Container />} />
         <Route path='/About' element={<About />} />
       </Routes>
       <Footer />
